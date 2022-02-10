f031d50d21553950f2c80006de686e9c8a4cebf5
gnoparus <surapong@gmail.com>
2022-02-10 23:27:39 +0700

    Refactored deploy 3 mocks code.

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,20 @@
+from brownie import Lottery, accounts, config, network
+from scripts.helpful_scripts import get_account, get_contract
+
+
+def deploy_lottery():
+    account = get_account()
+    lottery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Deployed lottery!")
+
+
+def main():
+    deploy_lottery()
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,73 @@
+from filecmp import DEFAULT_IGNORES
+from brownie import (
+    LinkToken,
+    VRFCoordinatorMock,
+    MockV3Aggregator,
+    network,
+    config,
+    accounts,
+)
+from web3 import Web3
+
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+DECIMALS = 8
+INITIAL_VALUE = 3200 * 10**10
+
+
+def get_account(index=None, id=None):
+
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        # use generaged fake account
+        return accounts[0]
+
+    # use mm account saved in brownie-config
+    return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+
+def get_contract(contract_name):
+    contract_type = contract_to_mock[contract_name]
+
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        # MockV3Aggregator[-1 ]
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type, _name, contract_address, contract_type.abi
+        )
+    return contract
+
+    # return {        _priceFeedAddress,
+    #      _vrfCoordinator,
+    #      _link,
+    #      _fee,
+    #      _keyHash,
+    #     "from": account}
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Deployed!")
