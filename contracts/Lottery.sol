22b2b9503a11b99f1523e1553bb5387f51a5c76d
gnoparus <surapong@gmail.com>
2022-02-10 15:35:26 +0700

    Added Lottery state enum and logic.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,19 +2,28 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
         // 50USD minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not enouth ETH!");
         players.push(msg.sender);
     }
@@ -30,7 +39,16 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.OPEN, "");
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+    }
 }
